0e57e1db39102e6b815b44f434501a3f1197be84
nehardt <nehardt@godaddy.com>
2015-09-14 14:05:23 -0700

    Add explicit errors for `after` and `action`

--- test/no-functions.js
+++ test/no-functions.js
@@ -8,14 +8,17 @@ actor.before('no-functions', function () {
   throw new Error('before shouldn\'t be reached');
 });
 
-assert.throws(function () {
-  actor.perform('no-functions', 'INVALID ACTION', 'INVALID CALLBACK');
-}, function (err) {
-  return err.message == 'last argument must be a function';
+actor.after('no-functions', function () {
+  throw new Error('after shouldn\'t be reached');
 });
 
 assert.throws(function () {
-  actor.perform('no-functions', function () {}, 'INVALID CALLBACK');
-}, function (err) {
-  return err.message == 'last argument must be a function';
-});
+  actor.perform('no-functions', 'INVALID ACTION', 'INVALID CALLBACK');
+}, /last argument must be a function/);
+
+assert.throws(function () {
+  function action () {
+  	throw new Error('action shouldn\'t be reached');
+  }
+  actor.perform('no-functions', action, 'INVALID CALLBACK');
+}, /last argument must be a function/);
